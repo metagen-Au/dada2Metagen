a282b1506eca74c9438469db3620157afaeec359
metagenAu <74326137+metagenAu@users.noreply.github.com>
2021-10-27 08:49:07 +1000

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -1,7 +1,7 @@
 pgks <- c("dada2","optparse","DECIPHER","ggplot2","gridExtra")
 
 for(i in seq_along(pgks)){
-  install.packages(pgks, repos='http://cran.us.r-project.org'))
+  install.packages(pgks, repos='http://cran.us.r-project.org')
 }
 
 # End 
