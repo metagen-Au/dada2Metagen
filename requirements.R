a176e1a68052da06440a1ba5fc4decab1e13f463
metagenAu <74326137+metagenAu@users.noreply.github.com>
2022-08-15 13:22:42 +1000

    minor tweak

--- requirements.R
+++ requirements.R
@@ -2,13 +2,13 @@ pgks <- c("optparse","gridExtra")
 pgks_bioc <- c("dada2","DECIPHER")
 
 for(i in seq_along(pgks)){
-  install.packages(pgks, repos='http://cran.us.r-project.org')
+  install.packages(pgks[i], repos='http://cran.us.r-project.org')
 }
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
 for(i in seq_along(pgks_bioc)){
-  BiocManager::install(pgks_bioc)
+  BiocManager::install(pgks_bioc[i])
 }
 
 
